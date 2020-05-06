e8f0d72d2d1d02e102cea29ecc9c00871135e944
MaxmilianKhn <maximilian.kuehn.00@googlemail.com>
2020-05-06 09:17:22 +0000

    Developed_isIsogram

--- src/isogram.c
+++ src/isogram.c
@@ -1,7 +1,17 @@
 #include "isogram.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 bool is_isogram(const char phrase[]){
-
-    return false;
+    int length=strlen(phrase);
+    for(int l1=0;l1<length;++l1){
+        for(int l2=l1+1;l2<length;++l2){
+            if(tolower(phrase[l1])==tolower(phrase[l2])){
+                if(phrase[l1]!=' ')return false;
+            }
+        }
+    }
+    return true;
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -1,17 +1,40 @@
 #include "vendor/unity.h"
 #include "../src/isogram.h"
+#include "vendor/unity_internals.h"
 #include <stdlib.h>
 
-void test_empty_string(void)
+void test_false_isogram(void)
 {
-   TEST_ASSERT_TRUE(is_isogram(""));
+   TEST_ASSERT_FALSE(is_isogram("AA"));
 }
+void test_true_isogram(void){
+   TEST_ASSERT_TRUE(is_isogram("Abcdefg"));
+}
+void test_word_with_upper_and_lower_letter(void){
+   TEST_ASSERT_FALSE(is_isogram("Aal"));
+}
+void spaces_should_not_count(void){
+    TEST_ASSERT_TRUE(is_isogram("Hi das funkt."))
+}
+void long_non_isograms(void){
+    TEST_ASSERT_FALSE(is_isogram("griechisch"));
+    TEST_ASSERT_FALSE(is_isogram("Hausaufgaben"));
+}
+/*void long_isograms(void){
+    TEST_ASSERT_TRUE();
+    TEST_ASSERT_TRUE();
+}*/
 
 int main(void)
 {
    UnityBegin("isIsogram");
 
-   RUN_TEST(test_empty_string);
+   RUN_TEST(test_false_isogram);
+   RUN_TEST(test_true_isogram);
+   RUN_TEST(test_word_with_upper_and_lower_letter);
+   RUN_TEST(spaces_should_not_count);
+   RUN_TEST(long_non_isograms);
+   //RUN_TEST(long_isograms);
 
    UnityEnd();
    return 0;
